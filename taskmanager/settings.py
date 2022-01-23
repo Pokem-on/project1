26896ab64bfcbae5bf457c5c75f592eab41cd9ba
Pokem-on <dmitry_ustenko@outlook.com>
2022-01-24 00:41:43 +0200

    version 2

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -141,3 +141,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
